c66ae073b6e46fa4870ec557bfabfe69aefd94e5
Javier Zazo Morillo <javiezaz@ucm.es>
2025-09-30 22:56:25 +0200

    Página hecha
    Faltaría el apartado de créditos cuando lo necesitemos y decorarlo

--- script.js
+++
@@ -1,9 +0,0 @@
-function devuelveTextoDeAlerta() {
-  return "uooooo! Vaya alerta";
-}
-
-function desaparece(nombre) {
-	var button = document.getElementById(nombre);
-  button.style.visibility='hidden';
-}
-
