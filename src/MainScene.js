2bbfd28e439886e59fa7c936136deca3b9bdad58
link10804k <62154757+link10804k@users.noreply.github.com>
2025-10-28 18:55:35 +0100

    Intento de zoom (Phaser roto)

--- src/MainScene.js
+++ src/MainScene.js
@@ -5,6 +5,35 @@ import Building from "./Building.js";
 export default class MainScene extends Phaser.Scene {
     constructor() {
         super({ key: "MainScene" });
+
+        this.scrollSpeed = 50; // Parámetro variable
+        this.zoomSpeed = 50; // Parámetro variable
+
+        // Eventos cámara
+        // Direction vectors (simple objects with x,y)
+        const Direction = {
+            UP: { x: 0, y: -1 },
+            DOWN: { x: 0, y: 1 },
+            LEFT: { x: -1, y: 0 },
+            RIGHT: { x: 1, y: 0 },
+        };
+
+        // Scroll
+        this.wKey = this.input.keyboard.addKey('W');
+        this.aKey = this.input.keyboard.addKey('A');
+        this.sKey = this.input.keyboard.addKey('S');
+        this.dKey = this.input.keyboard.addKey('D');
+        // Pass functions (don't call the methods immediately)
+        this.wKey.on("down", () => this.cameraScroll(Direction.UP));
+        this.aKey.on("down", () => this.cameraScroll(Direction.LEFT));
+        this.sKey.on("down", () => this.cameraScroll(Direction.DOWN));
+        this.dKey.on("down", () => this.cameraScroll(Direction.RIGHT));
+
+        // Zoom
+        this.iKey = this.input.keyboard.addKey('I');
+        this.oKey = this.input.keyboard.addKey('O');
+        this.iKey.on("down", () => this.cameraZoom(1));
+        this.oKey.on("down", () => this.cameraZoom(-1));
     }
 
     preload() { // Cargar recursos aquí
@@ -24,6 +53,14 @@ export default class MainScene extends Phaser.Scene {
         let building = new Building(this, 0, 0, "building", 0, 0, [], 0).setOrigin(0.5).setScale(0.35);
         building.setPosition(this.tiles[1][2].x, this.tiles[1][2].y);
         this.tiles[1][2].occupied = true;
+    }
+    cameraScroll(direction) {
+        direction *= this.scrollSpeed * game.time.elapsed/1000; // game.time.elapsed/1000 = deltaTime
+        this.cameras.main.scrollX += direction.x;
+        this.cameras.main.scrollY += direction.y;
+    }
+    cameraZoom(isZoomIn) {
+        this.cameras.main.zoom += this.zoomSpeed * isZoomIn * game.time.elapsed/1000;
     }
 
     createParcelas(){ //Crea parcelas
