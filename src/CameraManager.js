aaa28016362e0f10d4bb2702648de883d6939dd8
Javier Zazo Morillo <javiezaz@ucm.es>
2025-10-29 11:34:32 +0100

    Cámara fina

--- src/CameraManager.js
+++ src/CameraManager.js
@@ -4,74 +4,113 @@ export default class CameraManager {
         this.camera = camera;
         this.scene.events.on('preupdate', this.preupdate.bind(this));
 
-        this.scrollSpeed = 50; // Parámetro variable
-        this.zoomSpeed = 1.1; // Parámetro variable
+        this.scrollSpeed = 300; // Parámetro variable
+        this.zoomSpeed = 0.5; // Parámetro variable
+        this.minZoom = 0.75;
+        this.maxZoom = 1.5;
 
         this.camera.setBounds(-this.scene.tileWidth/2, -this.scene.tileHeight/2, this.scene.mapWidth, this.scene.mapHeight);
         //this.camera.setViewport(0, 0, this.scene.mapWidth, this.scene.mapHeight);
         this.camera.width = 800;
         this.camera.height = 600;
 
+        this.moveUp = false;
+        this.moveDown = false;
+        this.moveLeft = false;
+        this.moveRight = false;
+
+        this.zoomIn = false;
+        this.zoomOut = false;
+
         // Eventos cámara
+        
+        // Scroll
+        this.wKey = this.scene.input.keyboard.addKey('W');
+        this.aKey = this.scene.input.keyboard.addKey('A');
+        this.sKey = this.scene.input.keyboard.addKey('S');
+        this.dKey = this.scene.input.keyboard.addKey('D');
+        
+        this.wKey.on("down", () => this.moveUp = true);
+        this.aKey.on("down", () => this.moveLeft = true);
+        this.sKey.on("down", () => this.moveDown = true);
+        this.dKey.on("down", () => this.moveRight = true);
+        this.wKey.on("up", () => this.moveUp = false);
+        this.aKey.on("up", () => this.moveLeft = false);
+        this.sKey.on("up", () => this.moveDown = false);
+        this.dKey.on("up", () => this.moveRight = false);
+
+        //this.scene.input.keyboard.on('keydown-W', (event) => {
+        //    event.repeat === true;
+        //    this.cameraScroll(Direction.UP);
+        //});
+//
+        //this.scene.input.keyboard.on('keydown-A', (event) => {
+        //    event.repeat === true;
+        //    this.cameraScroll(Direction.LEFT);
+        //});
+//
+        //this.scene.input.keyboard.on('keydown-S', (event) => {
+        //    event.repeat === true;
+        //    this.cameraScroll(Direction.DOWN);
+        //});
+//
+        //this.scene.input.keyboard.on('keydown-D', (event) => {
+        //    event.repeat === true;
+        //    this.cameraScroll(Direction.RIGHT);
+        //});
+
+        // Zoom
+        this.iKey = this.scene.input.keyboard.addKey('I');
+        this.oKey = this.scene.input.keyboard.addKey('O');
+        this.iKey.on("down", () => this.zoomIn = true);
+        this.oKey.on("down", () => this.zoomOut = true);
+        this.iKey.on("up", () => this.zoomIn = false);
+        this.oKey.on("up", () => this.zoomOut = false);
+    }
+    cameraScroll(direction, dt) {
+        this.camera.scrollX += direction.x * this.scrollSpeed * dt;
+        this.camera.scrollY += direction.y * this.scrollSpeed * dt;
+    }
+    cameraZoomIn(dt) {
+        this.camera.zoom += this.zoomSpeed * dt //* this.scene.time.elapsed/1000
+        if (this.camera.zoom > this.maxZoom) {
+            this.camera.zoom = this.maxZoom;
+        }
+    }
+    cameraZoomOut(dt) {
+        this.camera.zoom -= this.zoomSpeed * dt //* this.scene.time.elapsed/1000
+        if (this.camera.zoom < this.minZoom) {
+            this.camera.zoom = this.minZoom;
+        }
+    }
+    preupdate(t, dt) {
+        dt /= 1000; // Convertir dt a segundos
+
         const Direction = {
             UP: { x: 0, y: -1 },
             DOWN: { x: 0, y: 1 },
             LEFT: { x: -1, y: 0 },
             RIGHT: { x: 1, y: 0 },
-        };
+        }
 
-        // Scroll
-        ///this.wKey = this.scene.input.keyboard.addKey('W');
-        ///this.aKey = this.scene.input.keyboard.addKey('A');
-        ///this.sKey = this.scene.input.keyboard.addKey('S');
-        ///this.dKey = this.scene.input.keyboard.addKey('D');
-        ///// Pass functions (don't call the methods immediately)
-        ///this.wKey.on("down", () => this.cameraScroll(Direction.UP));
-        ///this.aKey.on("down", () => this.cameraScroll(Direction.LEFT));
-        ///this.sKey.on("down", () => this.cameraScroll(Direction.DOWN));
-        ///this.dKey.on("down", () => this.cameraScroll(Direction.RIGHT));
-
-        this.scene.input.keyboard.on('keydown-W', (event) => {
-            event.repeat === true;
-            this.cameraScroll(Direction.UP);
-        });
-
-        this.scene.input.keyboard.on('keydown-A', (event) => {
-            event.repeat === true;
-            this.cameraScroll(Direction.LEFT);
-        });
-
-        this.scene.input.keyboard.on('keydown-S', (event) => {
-            event.repeat === true;
-            this.cameraScroll(Direction.DOWN);
-        });
-
-        this.scene.input.keyboard.on('keydown-D', (event) => {
-            event.repeat === true;
-            this.cameraScroll(Direction.RIGHT);
-        });
-
-        // Zoom
-        this.iKey = this.scene.input.keyboard.addKey('I');
-        this.oKey = this.scene.input.keyboard.addKey('O');
-        this.iKey.on("down", () => this.cameraZoomIn());
-        this.oKey.on("down", () => this.cameraZoomOut());
-    }
-    cameraScroll(direction) {
-        this.camera.scrollX += direction.x * this.scrollSpeed;
-        this.camera.scrollY += direction.y * this.scrollSpeed;
-    }
-    cameraZoomIn() {
-        this.camera.zoom *= this.zoomSpeed //* this.scene.time.elapsed/1000
-    }
-    cameraZoomOut() {
-        this.camera.zoom /= this.zoomSpeed //* this.scene.time.elapsed/1000
-    }
-    preupdate() {
-        //console.log("Time elapsed: " + this.scene.game.time.physicsElapsed);
-        //console.log("Camera scrollX: " + this.camera.scrollX);
-        //console.log("Camera scrollY: " + this.camera.scrollY);
-        //console.log("Camera zoom: " + this.camera.zoom);
+        if (this.moveUp) {
+            this.cameraScroll(Direction.UP, dt);
+        }
+        else if (this.moveDown) {
+            this.cameraScroll(Direction.DOWN, dt);
+        }
+        if (this.moveLeft) {
+            this.cameraScroll(Direction.LEFT, dt);
+        }
+        else if (this.moveRight) {
+            this.cameraScroll(Direction.RIGHT, dt);
+        }
+        if (this.zoomIn) {
+            this.cameraZoomIn(dt);
+        }
+        else if (this.zoomOut) {
+            this.cameraZoomOut(dt);
+        }
     }
 }
     
