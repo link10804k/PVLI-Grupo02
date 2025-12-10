124699f525f7e3df21bf6ba4823e1ea3390d8626
link10804k <62154757+link10804k@users.noreply.github.com>
2025-12-10 16:53:07 +0100

    Mayúsculas/minúsculas bien puestas en los loads

--- src/MainScene.js
+++ src/MainScene.js
@@ -39,16 +39,16 @@ export default class MainScene extends Phaser.Scene {
         this.load.image("panel", "assets/gameAssets/panel.jpg")
 
         // Productos no procesados
-        this.load.image("coffeeGrains_display", "assets/gameAssets/Coffee_display.png");
-        this.load.image("teaHerbs_display", "assets/gameAssets/TeaIcon.png");
-        this.load.image("cocoaBeans_display", "assets/gameAssets/cocoaBeans.png");
-        this.load.image("pumpkins_display", "assets/gameAssets/pumpkins.png");
-        this.load.image("dough_display", "assets/gameAssets/Dough.png");
-        this.load.image("sugar_display", "assets/gameAssets/sugar.png");
-        this.load.image("frozenBurgers_display", "assets/gameAssets/frozenBurgers.png");
-        this.load.image("frozenTacos_display", "assets/gameAssets/FrozenTaco.png");
-        this.load.image("frozenPizza_display", "assets/gameAssets/frozenPizza.png");
-        this.load.image("frozenPaella_display", "assets/gameAssets/frozenPaella.png");
+        this.load.image("CoffeeGrains_display", "assets/gameAssets/CoffeeGrainIcon.png");
+        this.load.image("TeaHerbs_display", "assets/gameAssets/TeaIcon.png");
+        this.load.image("CocoaBeans_display", "assets/gameAssets/cocoaBeans.png");
+        this.load.image("Pumpkins_display", "assets/gameAssets/pumpkins.png");
+        this.load.image("Dough_display", "assets/gameAssets/Dough.png");
+        this.load.image("Sugar_display", "assets/gameAssets/sugar.png");
+        this.load.image("FrozenBurgers_display", "assets/gameAssets/frozenBurgers.png");
+        this.load.image("FrozenTacos_display", "assets/gameAssets/FrozenTaco.png");
+        this.load.image("FrozenPizza_display", "assets/gameAssets/frozenPizza.png");
+        this.load.image("FrozenPaella_display", "assets/gameAssets/frozenPaella.png");
         // Productos procesados
         this.load.image("Coffee_display", "assets/gameAssets/CoffeeIcon.png")
         this.load.image("Tea_display", "assets/gameAssets/Tea.png")
