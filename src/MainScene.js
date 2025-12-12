3bf294b7e660a478c7ae9d111ec2899cf0c51a39
link10804k <62154757+link10804k@users.noreply.github.com>
2025-12-12 15:49:59 +0100

    cambio importacion cafeteria

--- src/MainScene.js
+++ src/MainScene.js
@@ -69,7 +69,7 @@ export default class MainScene extends Phaser.Scene {
         this.load.image("Pizza_display", "assets/gameAssets/Pizza.png");
         this.load.image("Paella_display", "assets/gameAssets/Paella.png");
         // Edificios
-        this.load.image("cafeteria", "assets/gameAssets/cafeteria.png")
+        this.load.image("cafeteria", "assets/gameAssets/Cafeteria.png")
 
         this.load.image("Farm_building", "assets/gameAssets/Tier1Farm.png");
         this.load.image("Farm_CoffeeGrains_texture", "assets/gameAssets/CoffeeFarm.png");
